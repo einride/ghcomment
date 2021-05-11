ca56ad0b6ec30f41080a64a4e8716aa22f5ed390
Jenny <jenny.eriksson@einride.tech>
2021-05-11 11:35:43 +0200

    feat: add initial CLI tool
    support for specifying signature key and creating and updating PR comments

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"crypto/sha1"
 	"flag"
+	"fmt"
 
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
@@ -10,26 +12,29 @@ import (
 
 func main() {
 
-	comment := flag.String("comment", "", "A PR Comment")
-	ghtoken := flag.String("ghtoken", "", "A github token")
-	ghrepo := flag.String("ghrepo", "", "A github repository")
-	prnumber := flag.String("prnumber", "", "A PR number")
-
-	_ = ghrepo
-	_ = prnumber
-	_ = comment
+	ghComment := flag.String("comment", "", "A PR Comment")
+	ghToken := flag.String("ghtoken", "", "A github token")
+	ghRepo := flag.String("ghrepo", "", "A github repository")
+	prNumber := flag.Int("prnumber", 0, "A Pull Request number")
+	signKey := flag.String("signkey", "", "A key used to create the signature")
 
 	flag.Parse()
 
+	signature := sha1.New()
+	_, err := signature.Write([]byte(*signKey))
+	if err != nil {
+		panic(err)
+	}
+
 	token := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *ghtoken},
+		&oauth2.Token{AccessToken: *ghToken},
 	)
 	httpClient := oauth2.NewClient(context.Background(), token)
 
 	client := githubv4.NewClient(httpClient)
 
 	gh := Github{client: client}
-	if err := gh.updatePrComment(context.Background(), 56, "einride", "prospective-shipper-service"); err != nil {
+	if err := gh.CommentPR(context.Background(), *prNumber, "einride", *ghRepo, *ghComment, fmt.Sprintf("%x", signature.Sum(nil))); err != nil {
 		panic(err)
 	}
 
