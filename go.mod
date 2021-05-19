2272aa719eb8e7105eaf17eed0e465d54cd4f818
Björn Lexell <bjorn.lexell@einride.tech>
2021-05-19 14:48:05 +0200

    feat: add support for using local git repo

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/einride/ghcomment
 go 1.16
 
 require (
+	github.com/go-git/go-git/v5 v5.3.0 // indirect
 	github.com/shurcooL/githubv4 v0.0.0-20201206200315-234843c633fa
 	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
--- main.go
+++ main.go
@@ -5,23 +5,39 @@ import (
 	"crypto/sha1"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
 )
 
 func main() {
 
-	ghComment := flag.String("comment", "", "A PR Comment")
-	ghToken := flag.String("ghtoken", "", "A github token")
-	ghRepo := flag.String("ghrepo", "", "A github repository")
-	prNumber := flag.Int("prnumber", 0, "A Pull Request number")
+	comment := flag.String("comment", "", "A PR Comment")
+	ghToken := flag.String("token", "", "A github token")
+	repo := flag.String("repo", "", "A github repository. In the format einride/<repository>. If empty the local git repo will be used")
+	pr := flag.Int("pr", 0, "A Pull Request number")
 	signKey := flag.String("signkey", "", "A key used to create the signature")
 
 	flag.Parse()
 
+	if *repo == "" {
+		path, err := retrieveLocalGitRepo()
+		if err != nil {
+			panic(fmt.Errorf("failed to parse local git repository: %v", err))
+		}
+		*repo = path
+	}
+	owner, name, err := parseGitPath(*repo)
+	if err != nil {
+		panic(err)
+	}
+
 	signature := sha1.New()
-	_, err := signature.Write([]byte(*signKey))
+	_, err = signature.Write([]byte(*signKey))
 	if err != nil {
 		panic(err)
 	}
@@ -34,8 +50,47 @@ func main() {
 	client := githubv4.NewClient(httpClient)
 
 	gh := Github{client: client}
-	if err := gh.CommentPR(context.Background(), *prNumber, "einride", *ghRepo, *ghComment, fmt.Sprintf("%x", signature.Sum(nil))); err != nil {
+	if err := gh.CommentPR(context.Background(), *pr, owner, name, *comment, fmt.Sprintf("%x", signature.Sum(nil))); err != nil {
 		panic(err)
 	}
+}
 
+func parseGitPath(path string) (owner string, name string, _ error) {
+	parts := strings.Split(path, "/")
+
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("Invalid repository format %s", path)
+	}
+	return parts[0], strings.TrimSuffix(parts[1], ".git"), nil
+}
+
+func retrieveLocalGitRepo() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("failed to get current directory: %v", err)
+	}
+	repo, err := git.PlainOpenWithOptions(dir, &git.PlainOpenOptions{
+		DetectDotGit: true,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to open .git directory: %v", err)
+	}
+	remotes, err := repo.Remotes()
+	if err != nil {
+		return "", fmt.Errorf("failed to list remotes: %v", err)
+	}
+	if len(remotes) != 1 {
+		return "", fmt.Errorf("found %d remotes expecting 1", len(remotes))
+	}
+	if err := remotes[0].Config().Validate(); err != nil {
+		return "", fmt.Errorf("invalid remote config: %v", err)
+	}
+	endpoint, err := transport.NewEndpoint(remotes[0].Config().URLs[0])
+	if err != nil {
+		return "", fmt.Errorf("unable to parse remote endpoint: %v", err)
+	}
+	if endpoint.Host != "github.com" {
+		return "", fmt.Errorf("not a github.com repository")
+	}
+	return endpoint.Path, nil
 }
